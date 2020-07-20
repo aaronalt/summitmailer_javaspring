ef9a2780050944101ccea5897232ad612829b24b
aaronalt <aaronalt07@gmail.com>
2020-07-20 17:30:09 +0300

    getCustomers list not mapped

--- src/main/java/summitmail/controllers/CustomerController.java
+++ src/main/java/summitmail/controllers/CustomerController.java
@@ -3,6 +3,8 @@ package summitmail.controllers;
 import org.apache.coyote.Response;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -12,6 +14,7 @@ import summitmail.services.CustomerService;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("unchecked")
@@ -44,6 +47,19 @@ public class CustomerController extends ApiController {
         Customer customer = service.getCustomer(id);
         if (customer == null) { return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response); }
         response.put("customer", customer);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity getCustomers(
+            @PathVariable(name = "resultsPerPage", required = false) int resultsPerPage,
+            @PathVariable(name = "page", required = false) int page) {
+        Map response = new HashMap<String, Map<String, ?>>();
+        int results = (Integer)resultsPerPage == 0 ? 20 : resultsPerPage;
+        int pageCount = (Integer)page == 0 ? 0 : page;
+        Map<String, ?> customers = service.getCustomers(resultsPerPage, page);
+        if (customers.isEmpty()) { return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response); }
+        response.put("customers", customers);
         return ResponseEntity.ok(response);
     }
 
